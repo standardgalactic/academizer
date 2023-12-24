c87be4d58789f8b93aa53b56b2253d47582054cc
standardgalactic <mechachleopteryx@gmail.com>
2023-12-24 17:54:55 -0400

    Imported from substrate

--- pushup.sh
+++ pushup.sh
@@ -7,39 +7,54 @@ base_directory="$PWD"
 academizer_directory="/mnt/c/Users/Mechachleopteryx/OneDrive/Documents/Github/academizer"
 
 # List of directories to process
-directories=("abraxas" "alphabet" "audiobooks" "autodoxx" "Centerfuge" "eclectric-oil" "example" "Haplopraxis/IFM" "keen-unicoder" "library" "logical-connectives" "mindgame" "mirror" "negentropy" "phonograph" "psychohistory" "quantum-soup" "standardgalactic.github.io" "substrate" "technobabble" "unfinished-thoughts" "xanadu" "zygomindfulness")
+directories=(("alphabet" "audiobooks" "Centerfuge" "eclectric-oil" "example" "Haplopraxis/IFM" "keen-unicoder" "logical-connectives" "mindgame" "mirror" "negentropy" "phonograph" "psychohistory" "quantum-soup" "standardgalactic.github.io" "technobabble" "unfinished-thoughts" "substrate" "xanadu" "zygomindfulness")
 
 
 # Loop through each specified directory
 for dir in "${directories[@]}"; do
-echo "Processing $dir"
+  echo "Processing $dir"
 
-# Full path to the source directory
-full_path="$base_directory/$dir"
+          # Full path to the source directory
+          full_path="$base_directory/$dir"
 
-# Check if directory exists
-if [ -d "$full_path" ]; then
-# Copy .mhtml, .html, .pdf, and .txt files to academizer
-cp "$full_path"/*.mhtml "$full_path"/*.html "$full_path"/*.pdf "$full_path"/*.txt "$academizer_directory"
+                  # Check if directory exists
+                  if [ -d "$full_path" ]; then
+                    # Copy .mhtml, .html, and .txt files to
+                    # academizer
+                    cp "$full_path"/*.mhtml "$full_path"/*.html "$full_path"/*.txt "$academizer_directory"
 
-# Change directory to academizer
-cd "$academizer_directory" || exit
+                                                # Change directory to
+                                                # academizer
+                          cd "$academizer_directory" || exit
 
-# Add all changes to git
-git add .
+                                                                # Add
+                                                                # all
+                                                                # changes
+                                                                # to git
+                                                                git add .
 
-# Commit the changes with a message
-git commit -m "Imported from $dir"
+                                                                                # Commit
+                                                                                # the
+                                                                                # changes
+                                                                                # with
+                                                                                # a message
+                                                                                git commit -m "Imported from $dir"
 
-echo "Acknowledging $dir" 
-sleep 5
+                                                                                echo "Acknowledging $dir" 
+                                                                                sleep 5
 
-# Uncomment the next line to enable git push
-git push
-  else
-    echo "Directory $full_path does not exist"
-  fi
-done
-
+                                                                                                    # Uncomment
+                                                                                                    # the
+                                                                                                    # next
+                                                                                                    # line
+                                                                                                    # to
+                                                                                                    # enable
+                                                                                                    # git
+                                                                                                    # push
+                                                                                                    git push
+                                                                                                  else
+                                                                                                    echo "Directory $full_path does not exist"
+                  fi
+                done
 
 
