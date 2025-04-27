1de097d1792e2a9f03de29e8afde0b1703031b04
standardgalactic <standardgalactic@protonmail.com>
2025-04-27 15:02:41 -0300

    Imported files from abraxas

--- academize.sh
+++ academize.sh
@@ -15,7 +15,7 @@ base_directory="$PWD"
 
 # List of directories to process
 directories=(
-"abraxas" "agora" "arcanum" "alphabet" "antivenom" "backward-compatibility" "brain" "bubble-city" "capstone" "Centerfuge" "circle-of-fifths" "cogniscium" "eclectric-oil" "example" "experiments" "ensign" "Haplopraxis/IFM" "hepastitium"  "human" "hyperspace" "keen-unicoder" "keyboard" "library" "logical-connectives" "lorax" "mindgame" "mirror" "negentropy" "phonograph" "pacer" "prototypes" "psychohistory" "quadrivium"
+"abraxas" "agora" "arcanum" "alphabet" "antivenom" "backward-compatibility" "brain" "bubble-city" "capstone" "Centerfuge" "circle-of-fifths" "codex" "cogniscium" "eclectric-oil" "example" "experiments" "ensign" "Haplopraxis/IFM" "hepastitium"  "human" "hyperspace" "keen-unicoder" "keyboard" "kitbash" "library" "logical-connectives" "lorax" "mindgame" "mirror" "negentropy" "phonograph" "pacer" "prototypes" "psychohistory" "quadrivium"
 "quantum-soup" "umbilicus" "secret-message"  "sitemap" "spherepop" "standardgalactic.github.io" "substrate" "supercube" "systada" "technobabble" "teleosemantics" "terminal-simulator" "theory" "transcript" "unscannable-interfaces" "vectorspace" "xanadu" "zetetics" "zygomindfulness" "xylomancy"
 )
 
--- pushup.sh
+++ pushup.sh
@@ -8,7 +8,7 @@ academizer_directory="$base_directory/academizer"
 
 # List of directories to process (full list restored)
 directories=(
-"abraxas" "agora" "arcanum" "alphabet" "antivenom" "backward-compatibility" "brain" "bubble-city" "capstone" "Centerfuge" "circle-of-fifths" "cogniscium" "eclectric-oil" "example" "experiments" "ensign" "Haplopraxis/IFM" "hepastitium"  "human" "hyperspace" "keen-unicoder" "keyboard" "library" "logical-connectives" "lorax" "mindgame" "mirror" "negentropy" "phonograph" "pacer" "prototypes" "psychohistory" "quadrivium"
+"abraxas" "agora" "arcanum" "alphabet" "antivenom" "backward-compatibility" "brain" "bubble-city" "capstone" "Centerfuge" "circle-of-fifths" "codex" "cogniscium" "eclectric-oil" "example" "experiments" "ensign" "Haplopraxis/IFM" "hepastitium"  "human" "hyperspace" "keen-unicoder" "keyboard" "kitbash" "library" "logical-connectives" "lorax" "mindgame" "mirror" "negentropy" "phonograph" "pacer" "prototypes" "psychohistory" "quadrivium"
 "quantum-soup" "umbilicus" "secret-message"  "sitemap" "spherepop" "standardgalactic.github.io" "substrate" "supercube" "systada" "technobabble" "teleosemantics" "terminal-simulator" "theory" "transcript" "unscannable-interfaces" "vectorspace" "xanadu" "zetetics" "zygomindfulness" "xylomancy" 
 )
 
